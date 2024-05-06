1db678e969487cfb438ee5a1169efaaa0ba6e2f7
koume80703 <kou.taka1147@gmail.com>
2024-05-06 14:28:39 +0900

    delete separateHiragana().

--- src/DAG.js
+++ src/DAG.js
@@ -1,8 +1,8 @@
-import romanPatternJSON from "./json/romanTypingParseDictionary.json" assert { type: "json" };
 import { combination } from "./combination.js";
+import Node from "./Node.js";
+import fs from "fs";
 
-const romanPattern = romanPatternJSON;
-// console.log("romanPattern = ", romanPattern);
+const romanPattern = JSON.parse(fs.readFileSync("/workspaces/typing/src/json/romanTypingParseDictionary.json"));
 
 class DAG {
     /*
@@ -12,11 +12,11 @@ class DAG {
     #head;
 
     constructor() {
-        this.#romanPatternDict = this.makeTypePatternDict();
+        this.#romanPatternDict = this.#makeTypePatternDict();
         this.#head = new Node(null, null);
     }
 
-    makeTypePatternDict() {
+    #makeTypePatternDict() {
         let rpd = {};
         romanPattern.forEach(element => {
             rpd[element.Pattern] = element.TypePattern;
@@ -26,52 +26,21 @@ class DAG {
     }
 
     generator(sentence) {
-        const parsedList = this.romanParse(sentence);
+        const parsedList = this.#romanParse(sentence);
 
         for (const parsedValue in parsedList) {
             const hiragana = parsedValue;
 
-
         }
     }
 
-    separateHiragana(hiragana) {
-        // hiraganaに入る文字列の最長は3なので長さ3の文字列を考えると
-        // "あいう"という文字列を分けるパターンの生成法は
-        // "あ1い2う"という文字の間のセパレータをそれぞれ選択するかどうか
-        // 言い換えれば2C2, 2C1のすべてのパターンで組み合わせを列挙し、
-        // セパレータの組み合わせごとに文字列を分割する方法を考えた。
+    /**
+     * 与えられたひらがなの文を最大3文字の単位に分け、各単位をリストとして格納する。
+     * @param {string} sentence - ひらがな文
+     * @returns string[] - ローマ字入力単位の文字列
+     */
+    #romanParse(sentence) {
 
-        let separetedPattern = [];
-        for (let i = 0; i < hiragana.length; i++) {
-            separetedPattern = separetedPattern.concat(combination(hiragana.length - 1, i));
-        }
-        let separatedHiraganaList = [];
-        separetedPattern.forEach(pattern => {
-            let separated = [];
-            // console.log("pattern:", pattern);
-            let str = "";
-            for (let i = 0; i < hiragana.length; i++) {
-                const ch = hiragana.charAt(i);
-                str += ch;
-                if (pattern.includes(i)) {
-                    separated.push(str);
-                    str = "";
-                }
-            }
-            if (str != "") {
-                separated.push(str);
-            }
-            separatedHiraganaList.push(separated);
-        })
-
-        return separatedHiraganaList;
-    }
-
-    romanParse(sentence) {
-        /*
-        与えられたひらがなの文を最大3文字の単位に分け、各単位をリストとして格納する。
-        */
         let idx = 0;
         let parsedList = [];
 
@@ -97,16 +66,8 @@ class DAG {
 
         return parsedList;
     }
-}
-export default DAG;
 
-class Node {
-    #value;
-    #next;
-
-    constructor(value, next) {
-        this.#value = value;
-        this.#next = next;
+    test(str) {
     }
 }
-
+export default DAG;
