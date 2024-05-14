160cf69e0fb36615e02f0e00e3bddb1a37e17de2
koume80703 <kou.taka1147@gmail.com>
2024-05-15 00:29:48 +0900

    We can create server from this file.

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+    console.log(req.method, req.url, req.httpVersion);
+
+    let filePath = path.join(
+        __dirname,
+        req.url === "/" ? "index.html" : req.url
+    );
+    let extname = path.extname(filePath);
+    let contentType = "text/html";
+
+    switch (extname) {
+        case ".js":
+            contentType = "application/javascript";
+            break;
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+    }
+
+    fs.readFile(filePath, function (error, data) {
+        res.writeHead(200, { "Content-Type": contentType });
+        res.write(data);
+        res.end();
+    });
+});
+
+const port = 8080;
+server.listen(port);
+console.log("Server listen on port " + port);
