9f4d6a1675cd26dbe087e9f635416ad334da242f
koume80703 <kou.taka1147@gmail.com>
2024-05-19 23:57:14 +0900

    Introduce game mode

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,9 +1,13 @@
 import GameManager from "./GameManager.js";
+import { GameMode } from "./GameManager.js";
 
 let gameManager;
 window.onload = function () {
     // 初期設定
-    gameManager = new GameManager();
+    const resParam = new URLSearchParams(window.location.search);
+    const mode = resParam.get("mode");
+
+    gameManager = new GameManager(mode);
 };
 
 window.addEventListener("keydown", (event) => {
