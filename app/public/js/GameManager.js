d7545795aab296314200f2d27a2f1e019a3f4526
koume80703 <kou.taka1147@gmail.com>
2025-05-01 00:19:20 +0900

    GameManagerが難易度情報を保持するように変更。 現時点では、画面から難易度情報を受け取ることができないため、一旦Easyとして固定値を格納済み。 今後対応予定

--- app/public/js/GameManager.js
+++ app/public/js/GameManager.js
@@ -18,6 +18,25 @@ class GameMode {
     }
 }
 
+class GameDifficulty {
+    static EASY = Symbol("easy");
+    static NORMAL = Symbol("normal");
+    static HARD = Symbol("hard");
+
+    static strToDifficulty(str) {
+        switch (str) {
+            case "easy":
+                return GameDifficulty.EASY;
+            case "normal":
+                return GameDifficulty.NORMAL;
+            case "hard":
+                return GameDifficulty.HARD;
+            default:
+                throw new Error("Illegal string argument of strToDifficulty()");
+        }
+    }
+}
+
 class GameManager {
     /**
      * @type {boolean}
@@ -39,27 +58,59 @@ class GameManager {
      * @type {GameMode}
      */
     _gameMode;
+    /**
+     * @type {GameDifficulty}
+     */
+    _gameDifficulty;
+    /**
+     * @param {Function}
+     */
+    _gameProcess;
 
     constructor(gameMode = null) {
         if (gameMode === null) {
             throw new Error("Not selected game mode");
         }
         this._isInGame = false;
+
         this._gameMode = GameMode.strToMode(gameMode);
+
+        // TODO ゲーム画面より難易度を選択可能とし、画面から難易度を受け取り後、その値を引数とする。
+        this._gameDifficulty = GameDifficulty.strToDifficulty("easy");
+
         this._typeManager = new TypeManager();
         this._viewManager = new ViewManager();
+
+        this._initGameProcess();
     }
 
     pushedKeyDown(event) {
+        // キーが押下されるたびにswitch文で条件分岐が走るのは効率が悪い
+        // コンストラクタの一回だけswitch文を利用し、それ以降は同じ関数を利用する方が効率がよい
+        // switch (this._gameMode) {
+        //     case GameMode.NORMAL:
+        //         this._normalGameProcess(event);
+        //         break;
+        //     case GameMode.ENDLESS:
+        //         this._endlessGameProcess(event);
+        //         break;
+        //     default:
+        //         throw new Error("Illegal game mode input");
+        // }
+
+        this._gameProcess(event);
+    }
+
+    _initGameProcess() {
         switch (this._gameMode) {
             case GameMode.NORMAL:
-                this._normalGameProcess(event);
+                this._gameProcess = this._normalGameProcess;
                 break;
             case GameMode.ENDLESS:
-                this._endlessGameProcess(event);
+                this._gameProcess = this._endlessGameProcess;
                 break;
             default:
-                throw new Error("Illegal game mode input");
+                break;
         }
     }
 
@@ -106,7 +157,7 @@ class GameManager {
 
     _viewReset() {
         const keyContentList = [
-            { start: "ゲーム開始はSpaceまたはEneter" },
+            { start: "ゲーム開始はSpaceまたはEnter" },
             { sentence: "" },
             { kanji: "" },
             { typedStr: "" },
